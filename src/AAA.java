104470883bc57d52c14dc83d1e441cd989e6daa4
max-yagch <49872246+max-yagch@users.noreply.github.com>
2019-04-23 19:22:10 +0800

    Update AAA.java

--- src/AAA.java
+++ src/AAA.java
@@ -7,21 +7,5 @@ public class AAA {
     public static void main(String[] args) {
         System.out.println("更新");
         System.out.println("开发游戏");
-        System.out.println("更换主题");
-        System.out.println("冲突冲突");
-	System.out.println("到此一游");
-	System.out.println("到此一游");
-	System.out.println("杨冲到此一游");
-	System.out.println("到此一游");
-	System.out.println("到此一游");
-        System.out.println("更新");
-        System.out.println("开发游戏");
-        System.out.println("更换主题");
-	System.out.println("到此一游");
-	System.out.println("到此一游");
-	System.out.println("到此一游");
-	System.out.println("到此一游");
-	System.out.println("杨冲修改");
-	System.out.println("杨冲修改");
     }
 }
