6afd07c54f128c856518951d6aea97c84fdb62e0
max-yagch <yagch@sina.cn>
2019-04-23 16:31:05 +0800

    新增类

---
+++ src/BBB.java
@@ -0,0 +1,11 @@
+
+/**
+ * @author max
+ * @projectName Namoal_To_Git
+ * @create 2019-04-23 16:29
+ */
+public class BBB {
+    public static void main(String[] args) {
+        System.out.println("uuuuuuuuuuu");
+    }
+}
